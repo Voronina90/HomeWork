4b0066a219c6e97e4abbf699e0546b2208ba5745
Voronina <sakura267@mail.ru>
2022-08-09 16:42:15 +0500

    Задача6

---
+++ Задача6/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+    
+if (A % 2 == 0)
+    {
+        Console.WriteLine("Четное");
+    }
+else
+    {
+        Console.WriteLine("Нечетное");
+    }
