9737d527a195c534fcbfdaf5db63b0b680faacd6
Voronina <sakura267@mail.ru>
2022-08-09 16:42:33 +0500

    Задача8

---
+++ Задача8/Program.cs
@@ -0,0 +1,12 @@
+﻿int I = 1;
+System.Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+while(I < N)
+{
+    I = I + 1;
+    if(I % 2 == 0)
+    {
+        Console.WriteLine(I);
+    }
+}
