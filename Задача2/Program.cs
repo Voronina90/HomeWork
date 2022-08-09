d35f399f3c8839b7d540e04e2e1e002cf887188c
Voronina <sakura267@mail.ru>
2022-08-09 16:41:28 +0500

    Задача2

---
+++ Задача2/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("Введите два числа");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int min = a;
+
+if (b > max) 
+{
+    max = b;
+} 
+
+if (b < min) 
+{
+    min = b; 
+}
+Console.Write("max=");
+Console.WriteLine(max);
+Console.Write("min=");
+Console.WriteLine(min); 
