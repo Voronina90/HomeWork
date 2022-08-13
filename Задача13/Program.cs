a9d59c6c9a1465a8bdc79afe1aaeb942b36fcee3
Voronina <sakura267@mail.ru>
2022-08-13 13:09:50 +0500

    Задача13

---
+++ Задача13/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.WriteLine("Введите число");
+
+int n = int.Parse(Console.ReadLine());
+int k = (int)Math.Log10(n)-2;
+Console.WriteLine(k<0? "Третьей цифры нет":(n%(int)Math.Pow(10, k+1)/(int)Math.Pow(10, k)). ToString());
+Console.WriteLine();
