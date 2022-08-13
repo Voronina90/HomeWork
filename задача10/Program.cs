db2ad750eea01147442c10f9bcf099e14d721aef
Voronina <sakura267@mail.ru>
2022-08-13 12:52:59 +0500

    Задача10

---
+++ задача10/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Введите трехзначное число.\nВыход из программы команда exit");
+while (true)
+{
+    string input = Console.ReadLine();
+    if (!input.Equals("exit"))
+        Console.WriteLine("{0}->{1}", input, input[1]);
+    else
+        break;
+}
