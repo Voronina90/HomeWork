77e0b7a6c553f701d53200d254454d1f1474f4bb
Voronina <sakura267@mail.ru>
2022-08-13 14:21:02 +0500

    Задача15

---
+++ Задача15/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите номер дня недели");
+int Day = Convert.ToInt32(Console.ReadLine());
+
+if ( Day == 6 || Day == 7)
+{
+    Console.WriteLine("Выходной");
+}
+else 
+{
+    if (Day > 0 && Day <=5)
+    Console.WriteLine("Рабочий");
+}
